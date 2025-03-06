09ceafff1b56d3f5fe8764ac69a8669baa606e84
Harry MacCode <emarh.harry.code@gmail.com>
2025-03-06 23:47:11 +0100

    Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import { Toaster } from 'react-hot-toast';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
+import EditProfile from './pages/EditProfile';
+import ForgotPassword from './pages/ForgotPassword';
+import ResetPassword from './pages/ResetPassword';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+      <Router>
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+          <Toaster position='top-right' />
+          <Routes>
+            <Route path='/register' element={<Register />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/profile' element={<Profile />} />
+            <Route path='/edit-profile' element={<EditProfile />} />
+            <Route path='/forgot-password' element={<ForgotPassword />} />
+            <Route path='/reset-password/:token' element={<ResetPassword />} />
+          </Routes>
+        </div>
+      </Router>
   );
 }
 
